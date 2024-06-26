f4eae53c6e5fafd5d34973a59d841368b5e7f269
“Batool <batool.taher124@gmail.com>
2024-06-26 13:28:49 +0300

    Added Styling

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/error", errorHandler)
+
+	fmt.Println("Server started at http://localhost:8080/")
+	err := http.ListenAndServe(":8080", nil)
+	if err!= nil{
+		log.Fatal("Error Excuting Server at 8080", err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html")
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "register.html")
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "login.html")
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "error.html")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	tmpl = "templates/" + tmpl
+	t, err := template.ParseFiles(tmpl)
+	if err != nil {
+		http.Error(w, "Template not found", http.StatusNotFound)
+		return
+	}
+	t.Execute(w, nil)
+}
